b916d1a99592f893360afb57a6ff7e8faab61f65
koayon <58336584+koayon@users.noreply.github.com>
2023-07-26 14:01:31 +0100

    Unidirectional Attention

--- gpt/attention.py
+++ gpt/attention.py
@@ -5,9 +5,16 @@ import torch as t
 from einops import rearrange
 from fancy_einsum import einsum
 from torch import nn
+from torch.nn import functional as F
 
 
 class UnidirectionalAttention(nn.Module):
+    """Unidirectional attention layer based on Attention is all you need.
+    Self-attention layer for decoder in transformer model.
+
+    Reference: https://arxiv.org/abs/1706.03762
+    """
+
     qkv_proj: nn.Linear
     output_proj: nn.Linear
     attn_dropout: nn.Dropout
@@ -26,8 +33,14 @@ class UnidirectionalAttention(nn.Module):
 
         self.num_heads = num_heads
         self.head_size = hidden_size // num_heads if head_size is None else head_size
-        self.qkv_proj = nn.Linear(hidden_size, (self.num_heads * self.head_size) * 3)
-        self.output_proj = nn.Linear((self.num_heads * self.head_size), hidden_size)
+
+        self.qkv_proj = nn.Linear(
+            hidden_size, (self.num_heads * self.head_size) * 3
+        )  # W_qkv
+        self.output_proj = nn.Linear(
+            (self.num_heads * self.head_size), hidden_size
+        )  # W_O
+
         self.attn_scale = 1.0 / math.sqrt(self.head_size)
         self.attn_dropout = nn.Dropout(dropout)
         self.resid_dropout = nn.Dropout(dropout)
@@ -38,11 +51,13 @@ class UnidirectionalAttention(nn.Module):
 
         Return: shape (batch, seq, hidden_size)
         """
-        _, S, H = x.shape
-        assert H == self.hidden_size
+        _, seq_length, hidden_size = x.shape
+        assert hidden_size == self.hidden_size
 
+        # Apply W_qkv to x to get q, k, v
         qkv = self.qkv_proj(x)  # (batch, seq, 3 * num_heads * head_size)
         q, k, v = t.split(qkv, (self.num_heads * self.head_size), dim=-1)
+
         q = rearrange(
             q, "batch seq (head dim) -> batch head seq dim", dim=self.head_size
         )
@@ -52,37 +67,33 @@ class UnidirectionalAttention(nn.Module):
         v = rearrange(
             v, "batch seq (head dim) -> batch head seq dim", dim=self.head_size
         )
-        if cache:
-            CB, C_HEADS, CS, C_HEADSIZE = cache.k.shape
-            assert CB == 1
-            assert C_HEADS == self.num_heads
-            assert C_HEADSIZE == self.head_size
-            if CS != 0:
-                assert (
-                    S == 1
-                ), "The cache is loaded. Should only pass one token at a time."
-            cache.k = k = t.cat([cache.k, k], dim=2)
-            cache.v = v = t.cat([cache.v, v], dim=2)
-        else:
-            CS = 0
-        # In the cache case, we're processing q from sequence position CS in the full sequence
-        q_ind = t.arange(CS, CS + S).unsqueeze(1)
-        k_ind = t.arange(0, CS + S).unsqueeze(0)
-        mask = (q_ind >= k_ind).to(x.device)
-        # For each query vector, a row of scores corresponding to each key vector
-        attn_scores = einsum(
-            "batch head seq_q dim, batch head seq_k dim -> batch head seq_q seq_k", q, k
-        )
-        attn_scores = attn_scores * self.attn_scale
-        neg_inf = t.tensor(-1e4, dtype=attn_scores.dtype, device=x.device)
-        attn_scores = t.where(mask, attn_scores, neg_inf)
-        probs = self.attn_dropout(attn_scores.softmax(dim=-1))
-        # For each query vector, the weighted average value vector
-        combined_v = einsum(
-            "batch head seq_q seq_k, batch head seq_k dim -> batch head seq_q dim",
-            probs,
+
+        # Combine q and k to get attention scores
+        q_k = t.einsum("bnih,bnjh->bnij", q, k)  # batch, num_heads, seq, seq
+        q_k *= self.attn_scale
+
+        mask = t.triu(t.ones_like(q_k)).to(x.device)  # seq, seq
+
+        # Use lower triangular mask for q_k matrix
+        masked_attention_scores = q_k.masked_fill(
+            mask == 0, float("-inf")
+        )  # batch, num_heads, seq, seq
+
+        attn_matrix = self.attn_dropout(
+            F.softmax(masked_attention_scores, dim=-1)
+        )  # seq, seq
+
+        # For each query vector, combine with the weighted average value vector
+        combined_with_v = einsum(
+            "batch head seq seq_i, batch head seq_i hidden_dim -> batch head seq hidden_dim",
+            attn_matrix,
             v,
-        )
-        combined_v = rearrange(combined_v, "batch head seq dim -> batch seq (head dim)")
-        out = self.output_proj(combined_v)
-        return self.resid_dropout(out)
+        )  # batch, num_heads, seq, hidden_size
+        combined_with_v = rearrange(
+            combined_with_v, "batch head seq hidden_dim -> batch seq (head dim)"
+        )  # batch, seq, hidden_size*num_heads
+
+        out = self.output_proj(combined_with_v)  # batch, seq, hidden_size
+        out = self.resid_dropout(out)
+
+        return out
