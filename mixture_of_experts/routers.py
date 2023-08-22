a14a03a8461943c757d215d6268a7f8206745be0
koayon <58336584+koayon@users.noreply.github.com>
2023-08-22 08:59:12 +0100

    Refactor and add docstrings

--- mixture_of_experts/routers.py
+++ mixture_of_experts/routers.py
@@ -17,29 +17,53 @@ class HashRouter(nn.Module):
     def __init__(
         self,
         *,
-        k: Optional[int] = None,
+        k: int = 1,
         config: MoEConfig,
     ):
         super().__init__()
         self.num_experts = config.num_experts
         self.vocab_size = config.vocab_size
-        self.k = k or 1
+        self.k = k
 
     def forward(
         self, input: Int[t.Tensor, "batch, seq"], hash: Int[t.Tensor, "vocab_size, k"]
-    ) -> Int[t.Tensor, "batch, seq"]:
+    ) -> Int[t.Tensor, "batch, seq, k"]:
         "Takes in token ids and a hashing function and returns the expert num that each token should be assigned to."
-        return hash[input]  # batch, seq, k
+        hashes = [hash[:, i][input] for i in range(self.k)]  # k list of seq, batch
 
-    def build_random_hash(self, seed: Optional[int] = None) -> t.Tensor:
+        out = t.stack(hashes, dim=-1)  # batch, seq, k
+        return out  # batch, seq, k
+
+    def build_random_hash(self, seed: int = 42) -> Int[t.Tensor, "vocab_size, k"]:
+        """Randomly assigns each token in vocabularly to k experts.
+
+        Returns
+        -------
+        t.Tensor
+            Hash function that maps tokens to k experts.
+            A lookup table which given a token id returns a list of k expert ids.
+        """
         g = t.Generator()
-        if seed is not None:
-            g.manual_seed(seed)
+        g.manual_seed(seed)
 
         hash = t.randint(
             high=self.num_experts, size=(self.vocab_size, self.k), generator=g
         )
         return hash
 
-    def build_balanced_hash(self) -> t.Tensor:
+    def build_balanced_hash(
+        self, token_frequencies: Int[t.Tensor, "vocab_size"]
+    ) -> t.Tensor:
+        """Assigns each token to k experts in a way that balances the number of tokens assigned to each expert.
+
+        Parameters
+        ----------
+        token_frequencies : t.Tensor
+            A tensor of shape (vocab_size,) where each element is the number of times a token appears in (a subset of) the training data.
+
+        Returns
+        -------
+        t.Tensor
+            Hash function that maps tokens to k experts.
+        """
         raise NotImplementedError
