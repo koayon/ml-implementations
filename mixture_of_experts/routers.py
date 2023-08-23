34bfeaae3642b391d7d663abc312a6c2a66080cd
koayon <58336584+koayon@users.noreply.github.com>
2023-08-23 04:15:11 +0100

    Make hash instance variable in router

--- mixture_of_experts/routers.py
+++ mixture_of_experts/routers.py
@@ -1,10 +1,11 @@
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Optional
 
 import torch as t
 from jaxtyping import Int
 from torch import nn
 
 from mixture_of_experts.config import MoEConfig
+from moet_experiment.moet_config import MoETConfig
 
 config = MoEConfig()
 
@@ -14,27 +15,26 @@ class HashRouter(nn.Module):
 
     Reference: https://arxiv.org/pdf/2106.04426.pdf"""
 
+    hash: Int[t.Tensor, "vocab_size k"]
+
     def __init__(
-        self,
-        *,
-        k: int = 1,
-        config: MoEConfig,
+        self, *, k: int = 1, config: MoEConfig | MoETConfig = config, num_experts: int
     ):
         super().__init__()
-        self.num_experts = config.num_experts
+        self.num_experts = num_experts
         self.vocab_size = config.vocab_size
         self.k = k
 
     def forward(
-        self, input: Int[t.Tensor, "batch, seq"], hash: Int[t.Tensor, "vocab_size, k"]
-    ) -> Int[t.Tensor, "batch, seq, k"]:
+        self, input: Int[t.Tensor, "batch seq"]
+    ) -> Int[t.Tensor, "batch seq k"]:
         "Takes in token ids and a hashing function and returns the expert num that each token should be assigned to."
-        hashes = [hash[:, i][input] for i in range(self.k)]  # k list of seq, batch
+        hashes = [self.hash[:, i][input] for i in range(self.k)]  # k list of seq, batch
 
         out = t.stack(hashes, dim=-1)  # batch, seq, k
         return out  # batch, seq, k
 
-    def build_random_hash(self, seed: int = 42) -> Int[t.Tensor, "vocab_size, k"]:
+    def build_random_hash(self, seed: int = 42):
         """Randomly assigns each token in vocabularly to k experts.
 
         Returns
@@ -49,11 +49,11 @@ class HashRouter(nn.Module):
         hash = t.randint(
             high=self.num_experts, size=(self.vocab_size, self.k), generator=g
         )
-        return hash
+        self.hash = hash
 
     def build_balanced_hash(
-        self, token_frequencies: Int[t.Tensor, "vocab_size"]
-    ) -> t.Tensor:
+        self, token_frequencies: Int[t.Tensor, "vocab_size k"]
+    ) -> None:
         """Assigns each token to k experts in a way that balances the number of tokens assigned to each expert.
 
         Parameters
