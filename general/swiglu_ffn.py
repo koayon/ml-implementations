5e87b44e142288b87aee61c438720b45300835b3
koayon <58336584+koayon@users.noreply.github.com>
2023-08-23 04:14:19 +0100

    Clean up swiglu

--- general/swiglu_ffn.py
+++ general/swiglu_ffn.py
@@ -5,7 +5,7 @@ from torch import nn
 from torch.nn import functional as F
 
 
-class SwiGLUFeedForward(nn.Module):
+class SwiGLUFFN(nn.Module):
     """Implements a SwiGLU feed forward network inspired by Swish paper: https://arxiv.org/pdf/1710.05941v1.pdf
 
     Llama 2 paper receommends SwiGLU as the activation function for FFN with hidden_dim = 2 / 3 * in_features * mult
@@ -14,23 +14,30 @@ class SwiGLUFeedForward(nn.Module):
     def __init__(
         self,
         in_features: int,
-        hidden_dim: int,
+        hidden_dim: Optional[int] = None,
+        dropout: float = 0.1,
     ):
+        """Implements a SwiGLU feed forward network inspired by Swish paper: https://arxiv.org/pdf/1710.05941v1.pdf
+
+        Llama 2 paper receommends SwiGLU as the activation function for FFN with hidden_dim = 2 / 3 * in_features * mult
+        """
         super().__init__()
         out_features = in_features
 
-        self.w1 = nn.Linear(
-            in_features=in_features, out_features=hidden_dim, bias=False
-        )
+        MULT = 4
+        RATIO = 2 / 3
+        up_dim = hidden_dim or int(in_features * RATIO * MULT)
 
-        self.w2 = nn.Linear(
-            in_features=hidden_dim, out_features=out_features, bias=False
-        )
+        self.w1 = nn.Linear(in_features=in_features, out_features=up_dim, bias=False)
+
+        self.w2 = nn.Linear(in_features=up_dim, out_features=out_features, bias=False)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        # FFN_ReLU would be simply w2(ReLU(w1_x)) instead we use SwiGLU:  w2(Swish(w1_x))
-        # Where swish = sigmoid(x) * x (the gating function)
+        """FFN_ReLU would be simply w2(ReLU(w1_x)) instead we use SwiGLU:  w2(Swish(w1_x))
+        Where swish = sigmoid(x) * x (the gating function)"""
 
-        swish = F.silu(self.w1(x))
+        swish = F.silu(self.w1(x))  #
         out = self.w2(swish)
+        out = self.dropout(out)
         return out
