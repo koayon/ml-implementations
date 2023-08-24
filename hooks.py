c768b0f52136415992e0c4dd67558d1b6c302a0e
koayon <58336584+koayon@users.noreply.github.com>
2023-08-24 23:55:43 +0100

    hooks to green

--- hooks.py
+++ hooks.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 import torch as t
 import torch.nn as nn
-import torch.nn.functional as F
+from torchvision.models import resnet
 
 
 def remove_hooks(module: t.nn.Module) -> None:
@@ -18,8 +18,12 @@ def remove_hooks(module: t.nn.Module) -> None:
 def add_print_io_shapes_hook(module: nn.Module) -> None:
     def fwd_hook(mod, input, output):
         print("Module:", mod.__class__.__name__)
-        print("Input shape:", input.shape)
-        print("Output shape:", output.shape)
+        print("Input shape:", input[0].shape) if isinstance(
+            input[0], t.Tensor
+        ) else print("Input type:", type(input[0]))
+        print("Output shape:", output.shape) if isinstance(output, t.Tensor) else print(
+            "Output type:", type(output)
+        )
         print()
 
     module.register_forward_hook(fwd_hook)
@@ -44,8 +48,15 @@ class FeatureExtractor(nn.Module):
         self.model = model
         self._features: t.Tensor
 
-        assert layer_name in self.model._modules.keys()
-        self.save_outputs_hook(dict([*self.model.named_modules()])[layer_name])
+        # assert layer_name in self.model._modules.keys()
+        try:
+            layer = dict([*self.model.named_modules()])[layer_name]
+            self.save_outputs_hook(layer)
+        except KeyError:
+            raise KeyError(
+                f"""Layer name {layer_name} not found in model.
+Available layers are: {dict([*self.model.named_modules()]).keys()}"""
+            )
 
     def save_outputs_hook(self, module: nn.Module) -> None:
         def fwd_hook(mod, input, output):
@@ -63,13 +74,26 @@ class LayerDiffRMS(nn.Module):
         self.model = model
         self._rms: float
 
-        assert layer_name in self.model._modules.keys()
-        self.layer_diff_rms_hook(dict([*self.model.named_modules()])[layer_name])
+        # assert layer_name in self.model._modules.keys()
+        # self.layer_diff_rms_hook(dict([*self.model.named_modules()])[layer_name])
+        try:
+            layer = dict([*self.model.named_modules()])[layer_name]
+            self.layer_diff_rms_hook(layer)
+        except KeyError:
+            raise KeyError(
+                f"""Layer name {layer_name} not found in model.
+Available layers are: {dict([*self.model.named_modules()]).keys()}"""
+            )
 
     def layer_diff_rms_hook(self, module: nn.Module) -> None:
         def fwd_hook(mod, input, output):
-            assert output.shape == input.shape
-            diff = output - input
+            try:
+                assert output.shape == input[0].shape
+            except AssertionError:
+                raise ValueError(
+                    f"Input and output shapes to layer {module} do not match."
+                )
+            diff = output - input[0]
             rms = t.sqrt(t.mean(diff**2)).item()
             self._rms = rms
 
@@ -77,3 +101,17 @@ class LayerDiffRMS(nn.Module):
 
     def forward(self, x: t.Tensor) -> t.Tensor:
         return self.model(x)
+
+
+def main():
+    model = resnet.resnet50()
+    x = t.randn(size=(1, 3, 224, 224))
+
+    model = VerboseModel(model)
+    out = model(x)
+
+    # print(resnet)
+
+
+if __name__ == "__main__":
+    main()
