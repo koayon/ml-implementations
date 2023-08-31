1e04b5d8a495664b11db4761c4cd609d70b49beb
Kola Ayonrinde <koayon@gmail.com>
2023-08-31 02:48:45 +0100

    Define device in general.init

---
+++ general/__init__.py
@@ -0,0 +1,11 @@
+import torch
+from torch import backends
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    torch.backends.cudnn.deterministic = True # type: ignore
+    torch.backends.cudnn.benchmark = False # type: ignore
+elif torch.backends.mps.is_available(): # type: ignore
+    device = torch.device("mps")
+else:
+    device = "cpu"
