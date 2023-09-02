650a21d13d80f8454d251a37a4c64ed06a8c67f9
Kola Ayonrinde <koayon@gmail.com>
2023-09-02 11:22:54 +0100

    Create TokenChoiceLayerCache

--- mixture_of_experts/cache.py
+++ mixture_of_experts/cache.py
@@ -33,16 +33,15 @@ class TokenChoiceLayerCache():
 
 # @typechecked
 class ExpertChoiceFullCache(Dict[str, ExpertChoiceLayerCache]):
-    """Cache containing the G, routing weights and assignments for each layer.
-
-    G is the softmaxed routing weights for the top k experts
-
-    token_assignments is the top k expert ids
-
-    routing_weights is the raw outputs of the routing model (before softmax)
-    """
-
     def __init__(self, moe_cache_dict: Dict[str, ExpertChoiceLayerCache]):
+        """Cache containing the G, routing weights and assignments for each layer.
+
+        G is the softmaxed routing weights for the top k experts
+
+        token_assignments is the top k expert ids
+
+        routing_weights is the raw outputs of the routing model (before softmax)
+        """
         super().__init__(moe_cache_dict)
 
     def __setitem__(self, idx: str, cache: ExpertChoiceLayerCache) -> None:
@@ -50,7 +49,7 @@ class ExpertChoiceFullCache(Dict[str, ExpertChoiceLayerCache]):
         super().__setitem__(idx, cache)
 
         # Make sure the cache has consistent shapes even when the number of experts per layer varies
-        self._pad_with_0s()
+        self._pad_with_negative1s()
 
     def __getitem__(self, __key: str) -> ExpertChoiceLayerCache:
         return super().__getitem__(__key)
@@ -75,21 +74,68 @@ class ExpertChoiceFullCache(Dict[str, ExpertChoiceLayerCache]):
     def num_experts(self) -> int:
         return max([layer_cache.G.shape[1] for idx, layer_cache in self.items()])
 
-    def _pad_with_0s(self) -> None:
+    def _pad_with_negative1s(self) -> None:
         """Some layers of the cache might have half the number of experts. In this case we want to pad this tensor with 0s so that they can stack together nicely"""
         for _, cache in self.items():
             if cache.G.shape[1] < self.num_experts:
                 # Get zeros to double the number of experts and pad the cache
-                zeros = t.zeros_like(cache.G)
-                cache.G = t.cat([cache.G, zeros], dim=1)
-                cache.token_assignments = t.cat([cache.token_assignments, zeros], dim=1)
+                negs = -t.ones_like(cache.G)
+                cache.G = t.cat([cache.G, negs], dim=1)
+                cache.token_assignments = t.cat([cache.token_assignments, negs], dim=1)
 
-                routing_weight_zeros = t.zeros_like(cache.routing_weights)
+                routing_weight_negs = -t.ones_like(cache.routing_weights)
                 cache.routing_weights = t.cat(
-                    [cache.routing_weights, routing_weight_zeros],
+                    [cache.routing_weights, routing_weight_negs],
                     dim=1,
                 )
 
-class TokenChoiceFullCache(Dict[str, ExpertChoiceLayerCache]):
-    """Will be similar to the above. Decoupling the two for later processing."""
-    raise NotImplementedError
+class TokenChoiceFullCache(Dict[str, TokenChoiceLayerCache]):
+    def __init__(self, moe_cache_dict: Dict[str, TokenChoiceLayerCache]):
+        """Cache containing the G, routing weights and assignments for each layer.
+
+        G is the softmaxed routing weights for the top k experts
+
+        token_assignments is the top k expert ids
+
+        routing_weights is the raw outputs of the routing model (before softmax)
+        """
+        super().__init__(moe_cache_dict)
+
+
+    def __setitem__(self, idx: str, cache: TokenChoiceLayerCache) -> None:
+        assert isinstance(cache, TokenChoiceLayerCache)
+        super().__setitem__(idx, cache)
+
+    def __getitem__(self, __key: str) -> TokenChoiceLayerCache:
+        return super().__getitem__(__key)
+
+    @property
+    def G(self) -> Float[t.Tensor, "layer batch_seq k"]:
+        return t.stack([cache.G for idx, cache in self.items()], dim=0)
+
+    @property
+    def expert_assignments(self) -> Int[t.Tensor, "layer batch_seq k"]:
+        return t.stack([cache.expert_assignments for idx, cache in self.items()], dim=0)
+
+    @property
+    def routing_weights_tensor(self) -> Float[t.Tensor, "layer batch*seq num_experts"]:
+        return t.stack([cache.routing_weights for idx, cache in self.items()], dim=0)
+
+    @property
+    def layer_indices(self) -> list[str]:
+        return list(self.keys())
+
+    @property
+    def num_experts(self) -> int:
+        return max([layer_cache.routing_weights.shape[-1] for idx, layer_cache in self.items()])
+
+    def _pad_with_negative1s(self) -> None:
+        """Some layers of the cache might have half the number of experts. In this case we want to pad this tensor with 0s so that they can stack together nicely"""
+        for _, cache in self.items():
+            if cache.routing_weights.shape[1] < self.num_experts:
+                # Get zeros to double the number of experts and pad the cache
+                routing_weight_negs = -t.ones_like(cache.routing_weights)
+                cache.routing_weights = t.cat(
+                    [cache.routing_weights, routing_weight_negs],
+                    dim=1,
+                )
