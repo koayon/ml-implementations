acc097c5e27a68645e63975152474784746ecb71
Kola Ayonrinde <koayon@gmail.com>
2023-09-11 20:44:35 +0100

    Expand out test cases

--- one_wide_moe/tests/test_shared_moe_model.py
+++ one_wide_moe/tests/test_shared_moe_model.py
@@ -8,11 +8,33 @@ from one_wide_moe.shared_moe_model import SharedParamsMoEModel
 
 tokenizer = AutoTokenizer.from_pretrained("roneneldan/TinyStories-8M")
 
+
+@pytest.mark.parametrize("ffn_dim_multiplier", [4, 6])
+@pytest.mark.parametrize("num_experts", [2, 4])
+@pytest.mark.parametrize("share_attention_layers", [True, False])
+@pytest.mark.parametrize("share_moe_layers", [True, False])
+@pytest.mark.parametrize("share_routers", [True, False])
 def test_shared_params_dense_model(
+    ffn_dim_multiplier: int,
+    num_experts: int,
+    share_attention_layers: bool,
+    share_moe_layers: bool,
+    share_routers: bool,
     batch_size: int = 2,
 ):
+    if (not share_moe_layers) and share_routers:
+        with pytest.raises(ValueError):
+            model = SharedParamsMoEModel(
+                ffn_dim_multiplier=ffn_dim_multiplier,
+                num_experts=num_experts,
+                share_attention_layers=share_attention_layers,
+                share_moe_layers=share_moe_layers,
+                share_routers=share_routers,
+            )
+        return
 
-    model = SharedParamsMoEModel()
+
+    model = SharedParamsMoEModel(ffn_dim_multiplier=ffn_dim_multiplier, num_experts = num_experts, share_attention_layers=share_attention_layers, share_moe_layers=share_moe_layers, share_routers = share_routers)
     # model.to(device)
 
     input_str = "Hello world"
