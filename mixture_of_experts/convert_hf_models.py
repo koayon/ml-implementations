0d1f640d7eb1ac3022cbe0d44962373b95641014
Kola Ayonrinde <koayon@gmail.com>
2023-09-25 12:45:55 -0400

    refactor

--- mixture_of_experts/convert_hf_models.py
+++ mixture_of_experts/convert_hf_models.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import torch as t
 import transformers
 from einops import rearrange
@@ -10,6 +12,7 @@ from transformers.modeling_outputs import (
 
 from mixture_of_experts.cache import (
     ExpertChoiceFullCache,
+    ExpertChoiceLayerCache,
     SoftTokenMergeLayerCache,
     TokenChoiceFullCache,
     TokenChoiceLayerCache,
@@ -22,19 +25,33 @@ tokenizer = AutoTokenizer.from_pretrained("google/switch-base-128")
 def get_layer_cache(
     layer_router_logits: t.Tensor, layer_str: str
 ) -> TokenChoiceLayerCache:
+    """_summary_
+
+    Parameters
+    ----------
+    layer_router_logits : t.Tensor
+        bs, num_experts
+    layer_str : str
+        _description_
+
+    Returns
+    -------
+    TokenChoiceLayerCache
+        Layer cache containing G, expert_assignments, P, routing_logits
+    """
     batch_size, seq_len, num_experts = layer_router_logits.shape
     layer_router_logits = rearrange(
         layer_router_logits, "batch seq expert ->  (batch seq) expert"
     )
     S = t.softmax(layer_router_logits, dim=-1)
 
-    # TODO: Extract to its own function
-    GEL = GroupExpertLayer(
+    # TODO: Extract _token_choice_routing_matrices to its own function
+    expert_layer = GroupExpertLayer(
         num_experts=num_experts,
         layer_id=layer_str,
         # c=None, k=None
     )
-    G, expert_assignments, P = GEL._token_choice_routing_matrices(
+    G, expert_assignments, P = expert_layer._token_choice_routing_matrices(
         S, batch_size, seq_len
     )
 
@@ -47,15 +64,13 @@ def get_layer_cache(
     return layer_cache
 
 
-class TokenChoiceMoE(PreTrainedModel):
-    def __init__(self, config):
+class TokenChoiceModelWrapper(PreTrainedModel):
+    def __init__(self, config, model_name: str = "google/switch-base-128"):
         super().__init__(config)
         self.config = config
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
-            "roneneldan/TinyStories-8M"
-        )
 
-        self.model = AutoModelForSeq2SeqLM.from_pretrained("google/switch-base-128")
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
     def forward(
         self,
@@ -70,7 +85,7 @@ class TokenChoiceMoE(PreTrainedModel):
         output_hidden_states=None,
         return_dict=None,
         output_router_probs=True,
-    ):
+    ) -> Tuple[Seq2SeqMoEModelOutput, TokenChoiceFullCache]:
         out: Seq2SeqMoEModelOutput = self.model(
             input_ids,
             attention_mask,
@@ -82,33 +97,33 @@ class TokenChoiceMoE(PreTrainedModel):
             output_attentions,
             output_hidden_states,
             return_dict,
+            output_router_probs=output_router_probs,
         )
 
-        encoder_router_logits = (
-            out.encoder_router_logits
-        )  # layers tuple[batch_size, seq len, num_experts]
-        decoder_router_logits = (
-            out.decoder_router_logits
-        )  # layers tuple[batch_size, seq_len, num_experts]
-        assert encoder_router_logits and decoder_router_logits
+        router_logits_types = []
+        if hasattr(out, "encoder_router_logits"):
+            router_logits_types.append(out.encoder_router_logits)
+            # layers tuple[batch_size, seq len, num_experts]
+
+        if hasattr(out, "decoder_router_logits"):
+            router_logits_types.append(out.decoder_router_logits)
+            # layers tuple[batch_size, seq len, num_experts]
+
+        if hasattr(out, "router_logits"):
+            router_logits_types.append(out.router_logits)  #  type: ignore
+            # layers tuple[batch_size, seq_len, num_experts]
 
         cache = {}
 
         # Convert this into a TokenChoiceLayerCache
-        for layer_idx, layer_router_logits in enumerate(encoder_router_logits):
-            layer_str = f"encoder_layer_{layer_idx}"
+        for router_logits in router_logits_types:
+            for layer_idx, layer_router_logits in enumerate(router_logits):
+                layer_str = f"encoder_layer_{layer_idx}"
 
-            layer_cache = get_layer_cache(layer_router_logits, layer_str)
+                layer_cache = get_layer_cache(layer_router_logits, layer_str)
 
-            cache[layer_str] = layer_cache
-
-        for layer_idx, layer_router_logits in enumerate(decoder_router_logits):
-            layer_str = f"decoder_layer_{layer_idx}"
-
-            layer_cache = get_layer_cache(layer_router_logits, layer_str)
-
-            cache[layer_str] = layer_cache
+                cache[layer_str] = layer_cache
 
         full_cache = TokenChoiceFullCache(cache)
 
-        return {}
+        return out, full_cache
