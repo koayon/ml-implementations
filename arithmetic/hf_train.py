82404e7a298c82af6674ba5c0d16ab1602fb7abb
Kola Ayonrinde <koayon@gmail.com>
2023-10-04 23:20:45 -0400

    Add trainer

--- arithmetic/hf_train.py
+++ arithmetic/hf_train.py
@@ -1,8 +1,12 @@
+from re import T
+
 import torch as t
 import torch.nn as nn
 from einops import rearrange
 from transformers import Trainer, TrainingArguments
 
+from arithmetic.model import ArithmeticNet
+from arithmetic.synthetic_data import get_dataset
 from general.character_level_tokenizer import CharTokenizer
 
 tokenizer = CharTokenizer()
@@ -16,15 +20,29 @@ class CustomTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         logits: t.Tensor
 
-        outputs = model(**inputs)
+        # Separate the tuples into two lists: inputs and outputs
+        input_ids = [item[0] for item in inputs]
+        target_ids = [item[1] for item in inputs]
+
+        # Stack the lists to create input and output tensors
+        input_tensor = t.stack(input_ids)  # batch, seq
+        output_tensor = t.stack(target_ids)  # batch, seq
+
+        outputs = model(encoder_input_ids=input_tensor, decoder_input_ids=output_tensor)
         logits, full_cache, idk_logits, pre_idk_logits = outputs
+
+        # TODO: Edit the generate function. What we need is for the model to autoregressively generate the whole output (which might be longer than the target and contain idk tokens)
+        # Then we put this through the below loss function.
+
+        print(logits.shape)
+        print(target_ids)
 
         # Get strip out all idk tokens from the output and instead give a penalty as n*pen for the number of idk tokens
 
         # logits [batch, seq, vocab_size]
         criterion = nn.CrossEntropyLoss(reduction="none")
         flattened_logits = rearrange(logits, "b s v -> (b s) v")
-        flattened_labels = rearrange(inputs["labels"], "b s -> (b s)")
+        flattened_labels = rearrange(target_ids, "b s -> (b s)")
 
         skipped_idk_tokens = (
             flattened_logits[:, tokenizer.idk_token_id] > 2.0
@@ -32,6 +50,9 @@ class CustomTrainer(Trainer):
         skipped_idk_tokens_list = skipped_idk_tokens.tolist()
 
         flattened_logits_list = flattened_logits.tolist()
+
+        print(skipped_idk_tokens_list)
+        assert False
 
         # Remove the columns in the logits that correspond to the idk tokens
         penalty = 0
@@ -46,3 +67,45 @@ class CustomTrainer(Trainer):
         loss = loss.sum() + penalty
 
         return (loss, outputs) if return_outputs else loss
+
+
+def main():
+    dataset = get_dataset()
+    model = ArithmeticNet(training=True)
+
+    training_args = TrainingArguments(
+        output_dir="./output",
+        num_train_epochs=10,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        warmup_steps=500,
+        weight_decay=0.01,
+        logging_dir="./logs",
+        logging_steps=10,
+        evaluation_strategy="steps",
+        load_best_model_at_end=True,
+        save_total_limit=1,
+        save_steps=10,
+        eval_steps=10,
+        no_cuda=False,
+        seed=42,
+        fp16=False,
+        fp16_opt_level="O1",
+    )
+
+    trainer = CustomTrainer(
+        model=model,
+        train_dataset=dataset,
+        eval_dataset=dataset,
+        tokenizer=tokenizer,
+        args=training_args,
+        idk_penalty=0.1,
+    )
+
+    trainer.train()
+
+    print(trainer.state)
+
+
+if __name__ == "__main__":
+    main()
