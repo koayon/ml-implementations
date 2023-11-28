1d95c44148e5796f1563aad2288734d581c033c7
Kola Ayonrinde <koayon@gmail.com>
2023-11-28 18:06:22 +0000

    Add dqn args

---
+++ rl/dqn/args.py
@@ -0,0 +1,54 @@
+import os
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class DQNArgs:
+    exp_name: str = os.path.basename(
+        globals().get("__file__", "DQN_implementation").rstrip(".py")
+    )
+    seed: int = 1
+    torch_deterministic: bool = True
+    cuda: bool = True
+    track: bool = False
+    wandb_project_name: str = "CartPoleDQN"
+    wandb_entity: Optional[str] = None
+    capture_video: bool = False
+    env_id: str = "CartPole-v1"
+    total_timesteps: int = 500000
+    learning_rate: float = 0.00025
+    buffer_size: int = 10000
+    gamma: float = 0.99
+    target_network_frequency: int = 500
+    batch_size: int = 128
+    start_e: float = 1.0
+    end_e: float = 0.05
+    exploration_fraction: float = 0.5
+    learning_starts: int = 10000
+    train_frequency: int = 10
+
+
+arg_help_strings = {
+    "exp_name": "the name of this experiment",
+    "seed": "seed of the experiment",
+    "torch_deterministic": "if toggled, `torch.backends.cudnn.deterministic=False`",
+    "cuda": "if toggled, cuda will be enabled by default",
+    "track": "if toggled, this experiment will be tracked with Weights and Biases",
+    "wandb_project_name": "the wandb's project name",
+    "wandb_entity": "the entity (team) of wandb's project",
+    "capture_video": "whether to capture videos of the agent performances (check out `videos` folder)",
+    "env_id": "the id of the environment",
+    "total_timesteps": "total timesteps of the experiments",
+    "learning_rate": "the learning rate of the optimizer",
+    "buffer_size": "the replay memory buffer size",
+    "gamma": "the discount factor gamma",
+    "target_network_frequency": "the timesteps it takes to update the target network",
+    "batch_size": "the batch size of samples from the replay memory",
+    "start_e": "the starting epsilon for exploration",
+    "end_e": "the ending epsilon for exploration",
+    "exploration_fraction": "the fraction of `total-timesteps` it takes from start-e to go end-e",
+    "learning_starts": "timestep to start learning",
+    "train_frequency": "the frequency of training",
+}
+toggles = ["torch_deterministic", "cuda", "track", "capture_video"]
