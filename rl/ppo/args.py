66bcd19921c8becb7b8383f12ebc5348191d10d6
Kola Ayonrinde <koayon@gmail.com>
2023-11-28 22:35:03 +0000

    Add PPO args

---
+++ rl/ppo/args.py
@@ -0,0 +1,30 @@
+import os
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class PPOArgs:
+    exp_name: str = os.path.basename(__file__).rstrip(".py")
+    seed: int = 1
+    torch_deterministic: bool = True
+    cuda: bool = True
+    track: bool = True
+    wandb_project_name: str = "PPOCart"
+    wandb_entity: Optional[str] = None
+    capture_video: bool = False
+    env_id: str = "CartPole-v1"
+    total_timesteps: int = 500000
+    learning_rate: float = 2.5e-4
+    num_envs: int = 4
+    num_steps: int = 128
+    gamma: float = 0.99
+    gae_lambda: float = 0.95
+    num_minibatches: int = 4
+    update_epochs: int = 4
+    clip_coef: float = 0.2
+    ent_coef: float = 0.01
+    vf_coef: float = 0.5
+    max_grad_norm: float = 0.5
+    batch_size: int = 512
+    minibatch_size: int = 128
