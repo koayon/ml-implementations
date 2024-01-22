d48d1449fad6a242c576f878c09b0e7484588309
Kola Ayonrinde <koayon@gmail.com>
2024-01-22 22:16:46 +0000

    Add HUpdate module

--- mamba/s6.py
+++ mamba/s6.py
@@ -7,10 +7,32 @@ from jaxtyping import Float
 from torch.nn import functional as F
 
 
+class HUpdater(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(
+        self, Ah: Optional[t.Tensor], Bx: t.Tensor, seq_num: Optional[int] = None
+    ) -> t.Tensor:
+        """Update the hidden state of the SSM.
+        Made into a module for hooking purposes. seq_num variable is used for ablations.
+
+        Returns
+        -------
+        h : t.Tensor
+            h_t
+        """
+        if Ah is None:
+            return Bx
+        else:
+            return Ah + Bx
+
+
 class SSM(nn.Module):
     def __init__(self):
         super().__init__()
         self.ssm_state: Optional[Float[t.Tensor, "batch input_dim hidden_dim"]] = None
+        self.h_updater = HUpdater()
 
     def _forward_recurrent(
         self,
@@ -34,7 +56,7 @@ class SSM(nn.Module):
         batch_size, seq_len, input_dim, hidden_dim = A.shape
 
         h: Float[t.Tensor, "batch seq_len input_dim hidden_dim"] = t.zeros(
-            batch_size, seq_len, input_dim, hidden_dim
+            (batch_size, seq_len, input_dim, hidden_dim), device=A.device
         )
 
         for seq_num in range(seq_len):
@@ -47,15 +69,17 @@ class SSM(nn.Module):
                 "batch input_dim hidden_dim, batch input_dim -> batch input_dim hidden_dim",
             )  # B x_t  # batch, input_dim, hidden_dim
 
+            A_h_t1: Optional[Float[t.Tensor, "batch input_dim hidden_dim"]] = None
             if seq_num:
                 A_h_t1 = einsum(
                     A[:, seq_num, :, :],
                     h[:, seq_num - 1, :, :],
                     "batch input_dim hidden_dim, batch input_dim hidden_dim -> batch input_dim hidden_dim",
                 )  # batch, input_dim, hidden_dim
-                h[:, seq_num, :] = A_h_t1 + B_xt  # h_t
-            else:
-                h[:, seq_num, :] = B_xt  # h_t
+
+            h[:, seq_num, :] = self.h_updater(A_h_t1, B_xt)  # h_t
+
+        self.ssm_state = h[:, -1, ...]  # batch, input_dim, hidden_dim
 
         y = einsum(
             C,
@@ -63,9 +87,55 @@ class SSM(nn.Module):
             "batch seq_len hidden_dim, batch seq_len input_dim hidden_dim -> batch seq_len input_dim",
         )  # C h_t  # batch, seq_len, input_dim
 
-        self.ssm_state = h[:, -1, ...]  # batch, input_dim, hidden_dim
-
         return y, h
+
+    def step(
+        self,
+        A: Float[t.Tensor, "batch input_dim hidden_dim"],
+        B: Float[t.Tensor, "batch input_dim hidden_dim"],
+        C: Float[t.Tensor, "batch hidden_dim"],
+        x: Float[t.Tensor, "batch input_dim"],
+    ) -> Float[t.Tensor, "batch seq_len dim"]:
+        """Complete one step of the SSM, given the ssm_state vectors.
+
+        Equations 2a and 2b in the Mamba paper.
+
+        Returns
+        -------
+        y : Float[t.Tensor, "batch seq_len input_dim"]
+        h : Float[t.Tensor, "batch seq_len input_dim hidden_dim"]
+        """
+        # h_t = A h_{t-1} + B x_t (element-wise multiplication)
+        # y_t = C h_t (matrix multiplication)
+
+        h = self.ssm_state
+
+        B_xt = einsum(
+            B,
+            x,
+            "batch input_dim hidden_dim, batch input_dim -> batch input_dim hidden_dim",
+        )
+        A_h_t1 = einsum(
+            A,
+            h,
+            "batch input_dim hidden_dim, batch input_dim hidden_dim -> batch input_dim hidden_dim",
+        )
+        # TODO: If we're ablating a state - we want to eliminate the B_xt term for sure.
+        # Should we also eliminate the A_h_t1 term? Probs yes because there's some dependence on x through A.
+        # But also might we want to have SOME dampening of the previous state?
+        # Otherwise we might be throwing it off its mental clock/position counter.
+        # Similarly C is dependant on x too! So we should probably ablate the output of the layer too.
+
+        h = self.h_updater(A_h_t1, B_xt)  # batch, input_dim, hidden_dim
+        self.ssm_state = h
+
+        y = einsum(
+            C,
+            h,
+            "batch hidden_dim, batch input_dim hidden_dim -> batch input_dim",
+        )  # C h_t  # batch, input_dim
+
+        return y
 
     def forward(
         self,
@@ -75,7 +145,11 @@ class SSM(nn.Module):
         x: Float[t.Tensor, "batch seq_len input_dim"],
     ) -> Float[t.Tensor, "batch seq_len dim"]:
         if self.ssm_state:
-            y = self.step(A, B, C, x)
+            x_final = x[:, -1, :]  # batch, input_dim
+            A_final = A[:, -1, :, :]  # batch, input_dim, hidden_dim
+            B_final = B[:, -1, :, :]  # batch, input_dim, hidden_dim
+            C_final = C[:, -1, :]  # batch, hidden_dim
+            y = self.step(A_final, B_final, C_final, x_final)
             return y
 
         else:
@@ -119,7 +193,6 @@ class S6(nn.Module):
         Returns
         -------
         A_disc : Float[t.Tensor, "batch seq_len input_dim hidden_dim"]
-
         B_disc : Float[t.Tensor, "batch seq_len input_dim hidden_dim"]
         """
         # Element-wise multiplication and exponentiation
@@ -168,3 +241,5 @@ class S6(nn.Module):
         y = self.ssm(A_disc, B_disc, C, x)  # batch, seq_len, input_dim
 
         return y
+
+    # TODO: Add step method for S6 (don't need to dicretize A and B on the whole sequence)
