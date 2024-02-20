b2fa3c9f62e2ff9261880db72278b4243f79988d
Kola Ayonrinde <koayon@gmail.com>
2024-02-20 16:51:49 +0000

    Update linear norm

--- mamba/attention_plots.py
+++ mamba/attention_plots.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 """
 For the branch with MambaInterp
@@ -17,7 +18,7 @@ import argparse
 
 import plotly.express as px
 import torch as t
-from einops import einsum, rearrange
+from einops import einsum, rearrange, repeat
 from jaxtyping import Float
 from loguru import logger
 from nnsight import NNsightModel, util
@@ -29,38 +30,44 @@ from sklearn.decomposition import NMF
 from transformers import AutoTokenizer
 
 
-def mean_abs_aggregation(
-    head_values: Float[t.Tensor, "layer source target input_dim"]
-) -> Float[t.Tensor, "layer source target"]:
-    return t.mean(head_values.abs(), dim=-1)
+def l1_aggregation(
+    head_values: Float[t.Tensor, "layer target source input_dim"]
+) -> Float[t.Tensor, "layer target source"]:
+    return t.sum(head_values.abs(), dim=-1)
 
 
 def nmf_aggregation(
-    head_values: Float[t.Tensor, "layer source target input_dim"]
-) -> Float[t.Tensor, "component source target"]:
+    head_values: Float[t.Tensor, "layer target source input_dim"]
+) -> Float[t.Tensor, "component target source"]:
     NUM_COMPONENTS = 16
     nmf_model = NMF(n_components=NUM_COMPONENTS, init="random", random_state=0)
 
     attn_values = rearrange(
-        head_values, "layer source target input_dim -> source target (layer input_dim)"
+        head_values, "layer target source input_dim -> target source (layer input_dim)"
     )
-    components = nmf_model.fit_transform(attn_values)  # source target component
+    components = nmf_model.fit_transform(attn_values)  # target source component
 
-    out = rearrange(components, "source target component -> component source target")
+    out = rearrange(components, "target source component -> component target source")
     return t.tensor(out)
 
 
-AGGREGATION_METHOD = {"mean_abs": mean_abs_aggregation, "nmf": nmf_aggregation}
+AGGREGATION_METHOD = {"l1": l1_aggregation, "nmf": nmf_aggregation}
 
 
-def linear_normalisation(values: t.Tensor) -> t.Tensor:
-    values = t.clip(values, 0)
-    return values / values.sum(dim=2, keepdim=True)
+def linear_normalisation(
+    values: Float[t.Tensor, "layer target source"], eps: float = 1e-6
+) -> Float[t.Tensor, "layer target source"]:
+    _num_layers, seq_len, _ = values.shape
+    row_sums = repeat(
+        t.sum(values, dim=2), "layer target -> layer target source", source=seq_len
+    )
+    normed_values = values / row_sums.clamp(min=eps)
+    return normed_values
 
 
 def get_layer_values(
     prompt: str, model: MambaInterp
-) -> tuple[DirectInvoker, Float[t.Tensor, "layer source target input_dim"]]:
+) -> tuple[DirectInvoker, Float[t.Tensor, "layer target source input_dim"]]:
     with model.invoke(
         prompt, fwd_args={"inference": True, "validate": True}
     ) as invoker:
@@ -84,21 +91,19 @@ def get_layer_values(
             discA: t.Tensor = mixer.ssm.discA.output
             discB: t.Tensor = mixer.ssm.discB.output
 
-            batch, input_dim, _seq_len, _state_dim = discA.shape
+            batch, input_dim, seq_len, _state_dim = discA.shape
 
-            source_values: list[list[t.Tensor]] = []
+            target_values: list[list[t.Tensor]] = []
 
             # Iterate through target tokens.
-            for target_token_idx in range(x.shape[2]):
-                target_values: list[t.Tensor] = []
+            for target_token_idx in range(seq_len):
+                source_values: list[t.Tensor] = []
 
                 # Iterate through source tokens.
-                for source_token_idx in range(x.shape[2]):
+                for source_token_idx in range(seq_len):
                     # If source is after target token, it can't "see" it ofc.
                     if target_token_idx < source_token_idx:
-                        attention_heads_tensor = -float("inf") * t.ones(
-                            (batch, input_dim)
-                        )
+                        attention_heads_tensor = 0 * t.ones((batch, input_dim))
                     else:
                         discB_source = discB[
                             :, :, source_token_idx, :
@@ -127,39 +132,28 @@ def get_layer_values(
                         )  # batch, input_dim
                         attention_heads_tensor = attention_heads_tensor.cpu().save()  # type: ignore
 
-                    target_values.append(
+                    source_values.append(
                         attention_heads_tensor
                     )  # list[batch, input_dim]
 
-                source_values.append(target_values)  # list[list[batch, input_dim]]
+                target_values.append(source_values)  # list[list[batch, input_dim]]
 
-            layer_values.append(source_values)  # list[list[list[batch, input_dim]]]
+            layer_values.append(target_values)  # list[list[list[batch, input_dim]]]
 
     layer_values_list = util.apply(layer_values, post, Proxy)
 
-    layer_values_tensor = t.stack(
+    full_attention_tensor = t.stack(
         [
             t.stack([t.stack(target).cpu() for target in source]).cpu()
             for source in layer_values_list
         ]
-    ).cpu()  # layer, source, target, batch, input_dim
+    ).cpu()  # layer, target, source, batch, input_dim
 
-    logger.info(f"{layer_values_tensor.shape=}")
-    logger.debug(layer_values_tensor[0])
+    logger.info(f"{full_attention_tensor.shape=}")
 
-    full_attention_tensor = t.tensor(
-        layer_values_tensor
-    )  # layer, source, target, batch, input_dim
-    output = t.squeeze(full_attention_tensor, dim=3)  # layer, source, target, input_dim
+    output = t.squeeze(full_attention_tensor, dim=3)  # layer, target, source, input_dim
 
-    logger.debug(
-        rearrange(
-            output[0, ..., :5], "source target input_dim -> input_dim source target"
-        )
-    )
-
-    # output = output_proxy
-    # output: t.Tensor = util.apply(output_proxy, post, Proxy)
+    logger.info(f"{output[0, ..., 0]}")
 
     return invoker, output
 
@@ -172,8 +166,7 @@ def visualise_attn_patterns(
     show_in_browser: bool = False,
 ) -> None:
     fig: Figure = px.imshow(
-        values.T.flip(0, 1),
-        # values,
+        values,
         color_continuous_midpoint=0.0,
         color_continuous_scale="RdBu",
         labels={"x": "Source Token", "y": "Target Token"},
@@ -184,7 +177,8 @@ def visualise_attn_patterns(
 
     fig.write_image(os.path.join(out_path, f"{name}.png"))
     if show_in_browser:
-        fig.show()
+        # fig.show()
+        fig.write_html(os.path.join(out_path, f"{name}.html"))
 
 
 def get_token_labels(model: NNsightModel, invoker: DirectInvoker) -> list[str]:
@@ -204,7 +198,7 @@ def main(
     prompt: str = "The capital of France is Paris",
     out_path: str = "./mamba/attn",
     repo_id: str = "state-spaces/mamba-130m",
-    aggregation: str = "mean_abs",
+    aggregation: str = "l1",
     normalisation: str = "linear",
 ):
     tokenizer = AutoTokenizer.from_pretrained(
@@ -225,10 +219,13 @@ def main(
         if aggregation == "nmf":
             grouping = "component"
     else:
-        logger.warning("Invalid aggregation method, using mean_abs instead")
-        aggregation_fn = mean_abs_aggregation
+        logger.warning("Invalid aggregation method, using l1 instead")
+        aggregation_fn = l1_aggregation
 
     values = aggregation_fn(full_attention_tensor)  # layer|component, source, target
+
+    logger.info(f"{values.shape=}")
+    logger.info(f"{values[0]=}")
 
     # Normalise row-wise along the target token dimension
     if normalisation == "linear":
@@ -254,6 +251,9 @@ def main(
 
 
 if __name__ == "__main__":
+
+    logger.remove()
+    logger.add(sys.stderr, level="INFO")
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
